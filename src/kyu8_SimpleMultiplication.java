f25a115bca8f39d4b3c3de400a1a2c0fdffec9fb
Kamil Wawer <Kamil.wawer92@gmail.com>
2019-01-14 16:24:22 +0100

    8 kyu - Simple multiplication

---
+++ src/kyu8_SimpleMultiplication.java
@@ -0,0 +1,21 @@
+// This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+public class kyu8_SimpleMultiplication {
+
+    public static void main(String[] args) {
+        int n = 10;
+        simpleMultiplication(n);
+    }
+
+    public static int simpleMultiplication(int n) {
+        int result = 0;
+        //your code here
+        if (n % 2 == 0) {
+            result = n * 8;
+        } else {
+            result = n * 9;
+        }
+        return result;
+    }
+}
+

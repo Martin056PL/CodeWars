c1391be60320cd690019e8ec358d4669a5eaf7dd
Kamil Wawer <Kamil.wawer92@gmail.com>
2019-01-15 15:39:39 +0100

    8 kyu - Function 3 - multiplying two numbers

---
+++ src/kyu8_MultiplyingTwoNumbers.java
@@ -0,0 +1,14 @@
+// This function has to be called multiply and needs to take two numbers as arguments,
+// and has to return the multiplication of the two arguments.
+
+public class kyu8_MultiplyingTwoNumbers {
+    public static void main(String[] args) {
+        int n1 = 12;
+        int n2 = 13;
+        multiply(n1,n2);
+    }
+
+    public static int multiply(int num1, int num2) {
+        return num1*num2;
+    }
+}

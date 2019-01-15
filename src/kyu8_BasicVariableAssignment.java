3d2c3f160f66bc310245389e670247db650bcbb3
Kamil Wawer <Kamil.wawer92@gmail.com>
2019-01-15 15:55:50 +0100

    8 kyu - Basic Variable Assignment (refactor)

--- src/kyu8_BasicVariableAssignment.java
+++ src/kyu8_BasicVariableAssignment.java
@@ -1,3 +1,6 @@
+//This code should store "codewa.rs" as a variable called "name" but it's not working.
+//Can you figure out why?
+
 public class kyu8_BasicVariableAssignment {
 
     public static String a = "code";
